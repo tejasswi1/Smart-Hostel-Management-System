bc47dbb12f78c6fb94387a33a930effcdf39f22d
Tejaswi <tejaswi7985@gmail.com>
2026-09-06 19:21:18 +0530

    Connect frontend to deployed backend

--- Frontend/smart-hostel-erp/src/api/axios.js
+++ Frontend/smart-hostel-erp/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-hostel-management-system-1-h4aw.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // 🔐 Token auto-attach
